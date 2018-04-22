d8ba06285aa02eed3c9be7f298deec522a6b503f
anthonynahas <anthony.na@hotmail.de>
2018-04-22 20:16:24 +0200

    feat(project): added the source code

---
+++ src/module/component/lib.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-material-contacts-component',
+  templateUrl: './lib.component.html',
+  styleUrls: ['./lib.component.scss']
+})
+export class LibComponent {
+  description = 'Angular Library to manage contacts and users with a material deisgn';
+}
