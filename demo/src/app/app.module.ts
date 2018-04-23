35e009fb34423a55aac1230f635e717ca548397b
anthonynahas <anthony.na@hotmail.de>
2018-04-23 20:02:37 +0200

    demo(app): added browseranimations module

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -1,30 +1,32 @@
+import {NgModule} from '@angular/core';
+import {HttpModule} from '@angular/http';
+import {FormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
-import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppSharedModule } from './shared/shared.module';
-import { HomeModule } from './home/home.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppSharedModule} from './shared/shared.module';
+import {HomeModule} from './home/home.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
-    imports: [
-        // Add .withServerTransition() to support Universal rendering.
-        // The application ID can be any identifier which is unique on
-        // the page.
-        BrowserModule.withServerTransition({appId: 'ngx-material-contacts-demo-id'}),
-        FormsModule,
-        HttpModule,
-        AppRoutingModule,
-        AppSharedModule,
-        HomeModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    // Add .withServerTransition() to support Universal rendering.
+    // The application ID can be any identifier which is unique on
+    // the page.
+    BrowserModule.withServerTransition({appId: 'ngx-material-contacts-demo-id'}),
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule,
+    AppSharedModule,
+    HomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
