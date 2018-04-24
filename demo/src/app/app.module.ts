e0966c0741f9e969c1b345c0f68e7eedb8f0ce32
anthonynahas <anthony.na@hotmail.de>
2018-04-24 20:42:42 +0200

    fix(project): added reactive forms module (required)

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
 import {HttpModule} from '@angular/http';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
@@ -20,6 +20,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     BrowserModule.withServerTransition({appId: 'ngx-material-contacts-demo-id'}),
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     AppRoutingModule,
     AppSharedModule,
